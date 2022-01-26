755820c56cb4759ac5c0837bb253c0cda71acaa8
smothers15 <dylo@fresnou.org>
2022-01-26 08:55:01 -0800

    slide 24

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const courseController = require('../controllers/courseController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/courses', courseController.viewAll);
+router.get('/courses/profile/:id', courseController.viewProfile);
 module.exports = router;
