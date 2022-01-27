044ab65f7bbc30f982c5a2d2fd184f5b16ddbba3
smothers15 <dylo@fresnou.org>
2022-01-27 08:35:42 -0800

    part 1 complete

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,4 +1,5 @@
 const {Course} = require('../models');
+const departments = ['Math', 'English', 'Music', 'Art', 'PE', 'World Language', 'Social Studies', 'Science'].sort();
 
 module.exports.viewAll = async function(req, res) {
     const courses = await Course.findAll();
@@ -8,4 +9,53 @@ module.exports.viewAll = async function(req, res) {
 module.exports.viewProfile = async function(req, res) {
     const course = await Course.findByPk(req.params.id);
     res.render('course/profile', {course});
+}
+
+module.exports.renderEditForm = async function(req, res) {
+    const course = await Course.findByPk(req.params.id);
+    res.render('course/edit', {course, departments});
+}
+
+module.exports.updateCourse = async function(req, res) {
+    const course = await Course.update({
+        name: req.body.name,
+        department: req.body.department,
+        instructor_name: req.body.instructor_name,
+        description: req.body.description
+    },{
+        where: {
+            id:req.params.id
+        }
+    });
+    res.redirect(`/courses/profile/${req.params.id}`);
+}
+
+module.exports.renderAddForm = async function(req, res) {
+    const course = {
+        name: '',
+        department: departments[0],
+        instructor_name: '',
+        description: ''
+    }
+    res.render('course/add', {course, departments});
+
+}
+
+module.exports.addCourse = async function(req, res) {
+    const course = await Course.create({
+        name: req.body.name,
+        department: req.body.department,
+        instructor_name: req.body.instructor_name,
+        description: req.body.description
+    });
+    res.redirect(`/courses/profile/${course.id}`);
+}
+
+module.exports.deleteCourse = async function(req, res) {
+    await Course.destroy({
+        where: {
+            id:req.params.id
+        }
+    });
+    res.redirect('/courses');
 }
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/courses', courseController.viewAll);
 router.get('/courses/profile/:id', courseController.viewProfile);
+router.get('/courses/edit/:id', courseController.renderEditForm);
+router.post('/courses/edit/:id', courseController.updateCourse);
+router.get('/courses/add', courseController.renderAddForm);
+router.post('/courses/add', courseController.addCourse);
+router.get('/courses/delete/:id', courseController.deleteCourse);
 module.exports = router;
